fd30564dad4fa3ea6c4217e8dd80f97ac5cc8935
Lushianna <pllchb@hotmail.com>
2022-11-25 06:47:33 -0500

    second commit

---
+++ funciones.js
@@ -0,0 +1,131 @@
+const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      onlyCountries: ["pe", "co", "ar", "br", "cl", "mx", "es"],
+      separateDialCode: "true",
+      
+    initialCountry: "pe",
+    geoIpLookup: getIp,
+    utilsScript:
+      "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+   });
+   
+
+   const info = document.querySelector(".alert-info");
+
+   function process(event) {
+    event.preventDefault();
+   
+    const phoneNumber = phoneInput.getNumber();
+   
+    info.style.display = "";
+    info.innerHTML = `El formato de teléfono debe ser: <strong>${phoneNumber}</strong>`;
+   };
+
+
+
+   function getIp(callback) {
+    fetch('https://ipinfo.io/json?token=<your token>', { headers: { 'Accept': 'application/json' }})
+      .then((resp) => resp.json())
+      .catch(() => {
+        return {
+          country: 'us',
+        };
+      })
+      .then((resp) => callback(resp.country));
+   }
+
+   const dropdowns = document.querySelectorAll('.dropdown');
+
+   dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+ 
+
+   select.addEventListener('click', ()=>{
+    select.classList.toggle('select-clicked');
+    caret.classList.toggle('caret-rotate');
+    menu.classList.toggle('menu-open');
+   });
+
+   options.forEach(option => {
+    option.addEventListener('click', ()=>{
+      selected.innerText = option.innerText;
+      select.classList.remove('select-clicked');
+      caret.classList.remove('caret-rotate');
+      menu.classList.remove('menu-open');
+      options.forEach(option =>{
+        option.classList.remove('active');
+      });
+      option.classList.add('active');
+    });
+   });
+
+  });
+
+
+  let p = document.getElementById("LiteClick"); // Obtiene el elemento con el id LiteClick
+  p.onclick = muestraAlerta; // Agrega función onclick al elemento
+    
+  function muestraAlerta(evento) {
+
+    document.querySelector("#details-aux-tit-d1").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d1").style.color = "#9da0bb";
+
+    document.querySelector("#details-aux-tit-d2").innerHTML="1";
+    document.querySelector("#details-aux-tit-d2").style.color = "#9da0bb";
+
+    document.querySelector("#details-aux-tit-d3").innerHTML="Max. 3";
+    document.querySelector("#details-aux-tit-d3").style.color = "#9da0bb";
+
+    document.querySelector("#details-aux-tit-d4").innerHTML="--";
+    document.querySelector("#details-aux-tit-d4").style.color = "#9da0bb";
+
+    document.querySelector("#details-aux-tit-d5").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d5").style.color = "#9da0bb";
+  }
+
+  let p2 = document.getElementById("IndiClick"); // Obtiene el elemento con el id IndiClick
+  p2.onclick = muestraAlerta2; // Agrega función onclick al elemento
+    
+  function muestraAlerta2(evento) {
+    
+    document.querySelector("#details-aux-tit-d1").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d1").style.color = "#9da0bb";
+  
+    document.querySelector("#details-aux-tit-d2").innerHTML="1";
+    document.querySelector("#details-aux-tit-d2").style.color = "#9da0bb";
+
+    document.querySelector("#details-aux-tit-d3").innerHTML="Ilimitadas";
+    document.querySelector("#details-aux-tit-d3").style.color = "#9da0bb";
+
+    document.querySelector("#details-aux-tit-d4").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d4").style.color = "#9da0bb";
+
+    document.querySelector("#details-aux-tit-d5").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d5").style.color = "#9da0bb";
+  }
+
+  let p3 = document.getElementById("DuoClick"); // Obtiene el elemento con el id DuoClick
+  p3.onclick = muestraAlerta3; // Agrega función onclick al elemento
+    
+  function muestraAlerta3(evento) {
+    
+    document.querySelector("#details-aux-tit-d1").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d1").style.color = "#ffd166";
+
+    document.querySelector("#details-aux-tit-d2").innerHTML="2";
+    document.querySelector("#details-aux-tit-d2").style.color = "#ffd166";
+
+    document.querySelector("#details-aux-tit-d3").innerHTML="Ilimitadas";
+    document.querySelector("#details-aux-tit-d3").style.color = "#ffd166";
+
+    document.querySelector("#details-aux-tit-d4").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d4").style.color = "#ffd166";
+
+    document.querySelector("#details-aux-tit-d5").innerHTML="&#10004";
+    document.querySelector("#details-aux-tit-d5").style.color = "#ffd166";
+  }
