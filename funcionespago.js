48857c6bb55558e1de61ca54e571039973de14e5
Lushianna <pllchb@hotmail.com>
2022-12-16 21:33:02 -0500

    seventh commit

--- funcionespago.js
+++ funcionespago.js
@@ -51,21 +51,42 @@ function pagos(){
 
 pagos();
 
+const option1 = document.getElementById('option1')
+const option2 = document.getElementById('option2')
+const content1 = document.getElementById('content1')
+const content2 = document.getElementById('content2')
 
-const li = document.querySelectorAll('.li')
-const bloque = document.querySelectorAll('.bloque')
+let choose = 1
 
-li.forEach(( cadaLi, i)=>{
-    li[i].addEventListener('click',()=> {
-        li.forEach(( cadaauc, y )=>{
-            li[y].classList.remove('activo')
-            bloque[y].classList.remove('activo')
-        })
+const changeOption = () =>{
+    choose == 1 ? (
+        option1.classList.value = 'option option-active',
+        content1.classList.value = 'content content-active'
+    )
+    :(
+        option1.classList.value = 'option',
+        content1.classList.value = 'content'
+    )
 
-        li[i].classList.add('activo')
-        bloque[i].classList.add('activo')
-    })
-});
+    choose == 2 ? (
+        option2.classList.value = 'option option-active',
+        content2.classList.value = 'content content-active'
+    )
+    :(
+        option2.classList.value = 'option',
+        content2.classList.value = 'content'
+    )
+}
+
+option1.addEventListener('click', ()=>{
+    choose = 1
+    changeOption()
+})
+
+option2.addEventListener('click', ()=>{
+    choose = 2
+    changeOption()
+})
 
 //For Card Number formatted input
 var cardNum = document.getElementById('card');
